3cfb763ec1ca7e8c20754cd1f55bad4ac869fc30
Misha00025 <102412201+misha00025@users.noreply.github.com>
2024-08-15 16:23:00 +0300

    feat: add text formatting

--- frontend/src/components/Note/index.tsx
+++ frontend/src/components/Note/index.tsx
@@ -5,15 +5,44 @@ import { INote } from '../../utils/api';
 import { useNotes } from '../../store/NoteContext';
 
 
+// Функция для замены символов и декодирования HTML
+const decodeHtml = (html: string | undefined) => {
+    // Заменяем HTML-сущности на соответствующие символы
+    if (html === undefined)
+        return "";
+    const txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+};
+
+interface MultiLineTextProps {
+    text: string;
+  }
+  
+  const MultiLineText: React.FC<MultiLineTextProps> = ({ text }) => {
+    const lines = text.split('\n');
+  
+    return (
+      <div>
+        {lines.map((line, index) => (
+          <>{line}<br /></>
+        ))}
+      </div>
+    );
+  };
+
+
 export const Note = () => {
     const { activeNote } = useNotes();
+    const formattedMessage = decodeHtml(activeNote?.body);
   return (
     <div className='note'>
         <header className='note-header'>
             {activeNote?.header}
         </header>
         <p className='note-text'>
-            {activeNote?.body}
+            <MultiLineText text = {formattedMessage} />
+
         </p>
         <div className='note-author'>
             <img src={activeNote?.author.photo}/><p className='note-author-text'>{activeNote?.author.first_name} {activeNote?.author.last_name}</p>
