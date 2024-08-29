153c0757a0f4dd3152b3b89eda4f2bdc12567d39
Misha00025 <102412201+misha00025@users.noreply.github.com>
2024-08-29 17:10:27 +0300

    feat: add groups urls

--- backend/app/api/v1/groups/processor.py
+++ backend/app/api/v1/groups/processor.py
@@ -1,14 +1,33 @@
 from flask import Request
 from app.processing.request_parser import *
-from app.model.VkUser import VkUser
-from app.model.Group import Group
-from app.processing.request_parser import get_group_id, get_user_id
+from app.model.GroupUsers import GroupUsers
+from app.model.UserGroups import UserGroups, Group
+from app.processing.request_parser import get_group_id, get_user_id, from_bot
+from app.status import ok, forbidden
 
 
 def get(group_id: int, rq: Request):
-    user_id = get_user_id(rq)
-    
+    d: dict
+    is_admin = True
+    group_id = str(group_id)
+    if not from_bot(rq):
+        user_id = get_user_id(rq)
+        user = UserGroups(user_id)
+        if group_id not in user.groups.keys():
+            return forbidden()
+        is_admin = user.is_admin(group_id)
+    group = GroupUsers(group_id)
+    d = group.to_dict()
+    if not is_admin:
+        d.pop("users")
+    return ok(d)
 
 
 def get_all(rq: Request):
-    pass
+    if from_bot(rq):
+        return get(get_group_id(rq), rq)
+    user_id = get_user_id(rq)
+    user = UserGroups(user_id)
+    d = {"groups": user.to_dict()["groups"]}
+    return ok(d)
+
--- backend/app/model/UserGroups.py
+++ backend/app/model/UserGroups.py
@@ -4,9 +4,13 @@ from app.database import user_group
 
 
 class UserGroups:
-    def __init__(self, user: VkUser) -> None:
+    def __init__(self, user: VkUser | str | int) -> None:
+        if user is int: 
+            user = str(user)
+        if user is str:
+            user = VkUser(user)
         self.info = user
-        self.groups: list = []
+        self.groups: dict = {}
         self.admin_in: list = []
 
     def _find(self):
@@ -17,14 +21,14 @@ class UserGroups:
             group_id = db_group[1]
             group = Group(group_id)
             is_admin = db_group[2]
-            self.groups.append(group)
+            self.groups[group_id] = group
             if is_admin:
                 self.admin_in.append(group_id)
 
     def to_dict(self):
         d = self.info.to_dict()
         groups = []
-        for group in self.groups:
+        for group in self.groups.items():
             group: Group
             d_group = group.to_dict()
             d_group["is_admin"] = self.is_admin(group.id)
