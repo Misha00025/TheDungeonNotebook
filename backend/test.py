f367a31f82604ffb894d2170b6ca98304746702b
Misha00025 <102412201+misha00025@users.noreply.github.com>
2024-08-30 12:12:27 +0300

    fix: notes and status codes

--- backend/test.py
+++ backend/test.py
@@ -15,38 +15,42 @@ headers_template = {"Content-Type": "application/json; charset=utf-8"}
 user_id = "tester"
 
 urls_get = [
-    "groups", "groups/-100", "groups/-101",
-    "notes", "notes/31", "notes/32",
+    "groups", "groups/-100", "groups/-101", "groups/-102",
+    "notes", "notes/31", "notes/32", "notes/33",
     "users", f"users/{user_id}"
 ]
 
 
-def user_get(headers, params, url):
+def user_get(headers, params, url, compact):
     full_url = site + url
     res = rq.get(url=full_url, headers=headers, params=params)
     try:
         response = res.json()
     except:
         response = res.text
-    text = f"REQUEST: {url}: Code: {res.status_code}"
+    text = f"REQUEST {res.status_code}: {url} "
     if res.status_code < 400:
-        text += f"\n   |- Response: {response}"
+        if not compact:
+            text += f"\n   |- Response: {response}"
+    else:
+        text += f"  Error: {response}"
     print(text)
 
 
-def user_get_tests(user_token, groups = ["-100"]):
+def user_get_tests(user_token, groups = ["-100"], compact = False):
     print(f"START: test with USER token: {user_token}\n")
     headers = headers_template.copy()
     headers[_at] = user_token
     for url in urls_get:
         for group_id in groups:
             payload = {"group_id": group_id}
-            user_get(headers, payload, url)
+            user_get(headers, payload, url, compact)
     print(f"\nEND: test with USER token: {user_token}\n\n")
     
 
 
 
 if __name__ == "__main__":
-    user_get_tests("1")
-    user_get_tests("2")
+    compact = bool(1)
+    user_get_tests("1", compact=compact)
+    user_get_tests("2", compact=compact)
