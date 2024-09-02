a52282ef192d22f1152df912b0e39e563533bddb
Misha00025 <102412201+misha00025@users.noreply.github.com>
2024-09-02 09:41:07 +0300

    fix: db connection

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,5 +4,16 @@ from flask_cors import CORS
 application = Flask(__name__)
 CORS(application)
 
+
+from database import get_instance
+from time import sleep
+while get_instance() is None:
+    print("Can't connect to database. Retrying after 10 seconds")
+    sleep(10)
+    continue
+
+
 import app.api.v0.router
 import app.api.v1
+
+
