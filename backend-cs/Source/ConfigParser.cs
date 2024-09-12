f913208281eb01601c260be7a9513ab6eecec57e
Misha00025 <102412201+misha00025@users.noreply.github.com>
2024-09-12 09:52:17 +0300

    feat: add db config

--- backend-cs/Source/ConfigParser.cs
+++ backend-cs/Source/ConfigParser.cs
@@ -7,20 +7,35 @@ namespace TdnApi.Configuration;
 
 public class ConfigParser
 {
+	private IniData _mainConfig;
+	private IniData _dbConfig;
 
-	public const string Connection = "server=localhost;port=3307;user=test_user;password=test-root;database=test_vk;"; 
+	public string Connection => GenerateConnection();
 
 	public ConfigParser(string filename){
 		var parser = new FileIniDataParser();
-		IniData data = parser.ReadFile(iniFilePath);
+		_mainConfig = parser.ReadFile(filename);
+		string dbFile = "configs/"+_mainConfig["DEFAULT"]["DbConnectionSettingsFile"];
+		_dbConfig = parser.ReadFile(dbFile);
+	}
 
-		// Получаем данные для авторизации
-		string username = data["Authentication"]["Username"];
-		string password = data["Authentication"]["Password"];
+	private string GenerateConnection()
+	{
+		var settings = _dbConfig["DATABASE"];
+		string host = settings["Host"];
+		string user = settings["User"];
+		string pass = settings["Password"];
+		string dbName = settings["DataBaseName"];
+		string port = "";
+		if (settings.ContainsKey("Port"))
+			port = $"port={settings["Port"]};";
+		
+		string connection = $"server={host};{port}user={user};password={pass};database={dbName};";
+		return connection;
 	}
 
 	public void ConfigDbConnections(DbContextOptionsBuilder opt)
-	{
+	{		
 		opt.UseMySql(Connection, new MySqlServerVersion(new Version(8, 0, 11)));
 	}
 }
