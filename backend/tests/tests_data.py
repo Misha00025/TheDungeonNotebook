cc361bfd2a5dd546cbc8f5684d3ac26d9ed17575
Misha00025 <102412201+misha00025@users.noreply.github.com>
2024-11-20 15:40:03 +0300

    chore: tests

--- backend/tests/tests_data.py
+++ backend/tests/tests_data.py
@@ -2,6 +2,7 @@ from .request_tests import headers_template, _st, _at
 
 DEBUG = "debug"
 
+default_debug = True
 
 class Test:
     def __init__(self,
@@ -11,12 +12,12 @@ class Test:
                  data: dict = {},
                  method: str = "GET",
                  requirement: int = 200,
-                 debug: bool = True
+                 debug: bool = None
                  ) -> None:
         self.request = request
-        self.params = params
-        self.params[DEBUG] = debug
-        self.headers = headers
+        self.params = params.copy()
+        self.params[DEBUG] = default_debug == True if debug is None else debug
+        self.headers = headers.copy()
         self.method = method
         self.data = data
 
@@ -50,11 +51,13 @@ CREATED = 201
 NOT_ALLOW = 405
 BAD = 400
 
-tests:list[Test]=[
+tests:list[Test]=[]
+tests.extend([
     Test(headers=uh, request=f"users/{mu}"),
     Test(headers=uh, request=f"users/{su}", requirement=FORBID),
     Test(headers=uh, request=f"users/{mu}/groups"),
-
+])
+tests.extend([
     Test(headers=uh, request=f"groups/{mg}"),
     Test(headers=uh, request=f"groups/{sg}"),
     Test(headers=uh, request=f"groups/{ssg}", requirement=FORBID),
@@ -66,22 +69,17 @@ tests:list[Test]=[
     Test(headers=uh, request=f"groups/{mg}", method="DELETE", requirement=OK),
     Test(headers=uh, request=f"groups/{sg}", method="DELETE", requirement=FORBID),
     Test(headers=uh, request=f"groups/{sg}", method="DELETE", requirement=FORBID),
-
+])
+tests.extend([
     Test(headers=uh, request=f"characters/{mc}", requirement=OK),
     Test(headers=uh, request=f"characters/{sc}", requirement=OK),
     Test(headers=uh, request=f"characters/{ssc}", requirement=FORBID),
-    Test(headers=uh, request=f"characters/{mc}", method="DELETE", requirement=FORBID), # OK
+    Test(headers=uh, request=f"characters/{mc}", method="DELETE", requirement=OK), # OK
     Test(headers=uh, request=f"characters/{sc}", method="DELETE", requirement=FORBID),
     Test(headers=uh, request=f"characters/{ssc}", method="DELETE", requirement=FORBID),
-    Test(headers=uh, request=f"characters/{mc}/notes", requirement=OK),
-    Test(headers=uh, request=f"characters/{sc}/notes", requirement=OK),
-    Test(headers=uh, request=f"characters/{ssc}/notes", requirement=FORBID),
     Test(headers=uh, request=f"characters/{mc}/inventories", requirement=OK),
     Test(headers=uh, request=f"characters/{sc}/inventories", requirement=OK),
     Test(headers=uh, request=f"characters/{ssc}/inventories", requirement=FORBID),
-    Test(headers=uh, request=f"characters/{mc}/notes", method="POST", requirement=OK), # CREATED
-    Test(headers=uh, request=f"characters/{sc}/notes", method="POST", requirement=FORBID),
-    Test(headers=uh, request=f"characters/{ssc}/notes", method="POST", requirement=FORBID),
     Test(headers=uh, request=f"characters/{mc}/inventories", method="POST", requirement=CREATED),
     Test(headers=uh, request=f"characters/{sc}/inventories", method="POST", requirement=FORBID),
     Test(headers=uh, request=f"characters/{ssc}/inventories", method="POST", requirement=FORBID),
@@ -91,11 +89,17 @@ tests:list[Test]=[
     Test(headers=uh, request=f"characters/{mc}/owners", method="DELETE", requirement=NOT_ALLOW),
     Test(headers=uh, request=f"characters/{mc}/owners/{su}", method="POST", requirement=OK),
     Test(headers=uh, request=f"characters/{mc}/owners/{su}", method="DELETE", requirement=OK),
-
+])
+tests.extend([
+    Test(headers=uh, request=f"characters/{mc}/notes", requirement=OK),
+    Test(headers=uh, request=f"characters/{sc}/notes", requirement=OK),
+    Test(headers=uh, request=f"characters/{ssc}/notes", requirement=FORBID),
+    Test(headers=uh, request=f"characters/{mc}/notes", method="POST", requirement=CREATED), # CREATED
+    Test(headers=uh, request=f"characters/{sc}/notes", method="POST", requirement=FORBID),
+    Test(headers=uh, request=f"characters/{ssc}/notes", method="POST", requirement=FORBID),
     Test(headers=uh, request=f"characters/{mc}/notes/1", requirement=OK),
     Test(headers=uh, request=f"characters/{mc}/notes/1", method="PUT", requirement=OK),
     Test(headers=uh, request=f"characters/{mc}/notes/1", method="DELETE", requirement=OK),
-    Test(headers=uh, request=f"characters/{sc}/notes/1", requirement=OK),
+    Test(headers=uh, request=f"characters/{sc}/notes/1", requirement=OK),    
     Test(headers=uh, request=f"characters/{sc}/notes/1", method="PUT", requirement=FORBID),
-    Test(headers=uh, request=f"characters/{sc}/notes/1", method="DELETE", requirement=FORBID),
-]
+])
