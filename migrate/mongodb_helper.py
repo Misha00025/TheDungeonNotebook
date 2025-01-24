6b2b915329582a63cddc6962c57e5107432baf49
Misha00025 <102412201+Misha00025@users.noreply.github.com>
2025-01-24 19:24:57 +0300

    fix: delete uuid from mongodb

--- migrate/mongodb_helper.py
+++ migrate/mongodb_helper.py
@@ -28,7 +28,6 @@ def insert_character_to_mongo(mongodb_uri, user, group, notes, user_items):
                 "amount": item['amount'],
             }) 
     document = {
-        "uuid": str(ObjectId()),
         "name": f"{user['first_name']} {user['last_name']}",
         "description": "some description",
         "notes": character_notes,
@@ -42,7 +41,6 @@ def insert_template_to_mongo(mongodb_uri, group):
     db = client[MONGO_DB_NAME]
     collection = db["templates"]  
     document = {
-        "uuid": str(ObjectId()),
         "name": f"Template {group['group_name']}",
         "description": "some description",
     }
@@ -54,7 +52,6 @@ def insert_item_to_mongo(mongodb_uri, item):
     db = client[MONGO_DB_NAME]
     collection = db["items"]  
     document = {
-        "uuid": str(ObjectId()),
         "name": f"{item['name']}",
         "description": f"{item['description']}"
     }
