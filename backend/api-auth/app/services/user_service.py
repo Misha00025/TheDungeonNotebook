d0bec2a7f4a942a28dcc3183018a7c31ce56b3d4
Misha00025 <102412201+misha00025@users.noreply.github.com>
2025-02-25 11:48:00 +0300

    fix: incorrect key value

--- backend/api-auth/app/services/user_service.py
+++ backend/api-auth/app/services/user_service.py
@@ -19,7 +19,7 @@ def create_or_update_user(user_id, user_info):
             UPDATE user SET first_name = %s, last_name = %s, photo_link = %s
             WHERE user_id = %s
         """
-        execute_query(update_query, (user_info['first_name'], user_info['last_name'], user_info['photo_link'], user_id))
+        execute_query(update_query, (user_info['first_name'], user_info['last_name'], user_info['photo_100'], user_id))
 
 def save_user_token(user_id, jwt_token):
     # Сохраняем токен в таблицу user_token
