0c82c9e22ef4d9b6d401a4f008a7a76b19914576
Misha00025 <102412201+misha00025@users.noreply.github.com>
2025-02-25 11:43:24 +0300

    fix: db connection in auth

--- backend/api-auth/utils/db.py
+++ backend/api-auth/utils/db.py
@@ -6,7 +6,7 @@ from config import connection_settings
 pool = ConnectionPool(**connection_settings)
 
 def execute_query(query, params=None):
-	with pool.connection() as conn:
+	with pool.get_connection() as conn:
 		with conn.cursor(DictCursor) as cursor:
 			cursor.execute(query, params)
 			result = cursor.fetchall()
