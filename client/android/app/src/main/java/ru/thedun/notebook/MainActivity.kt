67d6d2f44c408da6d04bbe70139737a697b3ccc6
Misha00025 <102412201+Misha00025@users.noreply.github.com>
2025-02-26 18:23:40 +0300

    feat: add notes activity for test

--- client/android/app/src/main/java/ru/thedun/notebook/MainActivity.kt
+++ client/android/app/src/main/java/ru/thedun/notebook/MainActivity.kt
@@ -1,20 +1,31 @@
 package ru.thedun.notebook
 
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_notes)
+        val myDataset = MutableList<String>(200, { i -> "Item $i"})
+
+        val viewManager = LinearLayoutManager(this)
+        val viewAdapter = NotesAdapter(myDataset)
+
+        val tvCharacterName: TextView = findViewById(R.id.tvCharacterName)
+        val llNotes: RecyclerView = findViewById(R.id.rvNotes)
+        llNotes.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
         }
     }
 }
