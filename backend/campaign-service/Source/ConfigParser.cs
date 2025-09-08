8dcac83eb2c3df3426d26d20bdc0549c8136dcce
Misha00025 <102412201+misha00025@users.noreply.github.com>
2025-09-08 16:55:58 +0300

    chore: add MONGO_DATABASE to .env

--- backend/campaign-service/Source/ConfigParser.cs
+++ backend/campaign-service/Source/ConfigParser.cs
@@ -10,6 +10,7 @@ public class ConfigParser
 {	
 	private string? _mongoConnectionString;
 	private string? _databaseName;
+	private string? _mongoDBName;
 	private string? _mysqlConnectionString;
 	
 	private string? _connection = null;
@@ -25,15 +26,17 @@ public class ConfigParser
 		_mongoConnectionString = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING");
 		_mysqlConnectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING");
 		_databaseName = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
+		_mongoDBName = Environment.GetEnvironmentVariable("MONGO_DATABASE");
 		if (_mongoConnectionString == null || _mysqlConnectionString == null || _databaseName == null)
 		{
-			
 			throw new Exception($"Can't find information to connect to databases:\n"+
 									$" |-mongo:{_mongoConnectionString}\n"+
 									$" |-mysql:{_mysqlConnectionString}\n"+
 									$" |-dbname: {_databaseName}"
 								);
 		}
+		if (_mongoDBName == null)
+			_mongoDBName = _databaseName;
 	}
 
 	public void ConfigDbConnections(DbContextOptionsBuilder opt)
@@ -46,7 +49,7 @@ public class ConfigParser
 		string dbName;
 		string connection;
 		connection = _mongoConnectionString!;
-		dbName = _databaseName!;
+		dbName = _mongoDBName!;
 		var settings = new MongoDbSettings
 		{
 			ConnectionString = connection,
