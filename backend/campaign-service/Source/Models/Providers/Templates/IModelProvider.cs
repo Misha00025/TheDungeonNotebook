290068f397996abc5028050bdec2d9f33c5093e3
Misha00025 <102412201+misha00025@users.noreply.github.com>
2025-09-22 13:00:27 +0300

    refactor: remove unused providers

--- backend/campaign-service/Source/Models/Providers/Templates/IModelProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace Tdn.Models.Providing;
-
-public interface IModelProvider<T>
-{
-	delegate void OnModelBuilded(T model);
-	event OnModelBuilded? ModelBuilded;
-	T? GetModel(string uuid);
-}
