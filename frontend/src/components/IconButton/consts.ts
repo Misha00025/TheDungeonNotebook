9ef87e627b649b3f9dd5b8ecf117bf5f52f6d296
Mikhail Nikolaev <102412201+Misha00025@users.noreply.github.com>
2026-01-09 15:45:34 +0300

    feat: remove frontend

--- frontend/src/components/IconButton/consts.ts
+++
@@ -1,5 +0,0 @@
-// @see https://codepen.io/sosuke/pen/Pjoqqp
-export const GREEN_FILTER =
-  "invert(45%) sepia(86%) saturate(3635%) hue-rotate(128deg) brightness(94%) contrast(101%)";
-export const RED_FILTER =
-  "invert(67%) sepia(47%) saturate(371%) hue-rotate(300deg) brightness(99%) contrast(100%)";
--- frontend/src/pages/Settings/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const SettingsPage = () => {
-  return <div>SettingsPage</div>;
-};
