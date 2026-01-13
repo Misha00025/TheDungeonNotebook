872505e99da79d017ebde004e9a7f5640cde0786
Misha00025 <102412201+Misha00025@users.noreply.github.com>
2026-01-13 15:20:09 +0300

    fix: remove unused data

--- backend/schemas-service/Source/ConfigParser.cs
+++ backend/schemas-service/Source/ConfigParser.cs
@@ -6,34 +6,17 @@ namespace Tdn.Configuration;
 public class ConfigParser
 {	
 	private string? _mongoConnectionString;
-	private string? _databaseName;
 	private string? _mongoDBName;
-	private string? _mysqlConnectionString;
-	
-	private string? _connection = null;
-	public string Connection { get 
-		{
-			if (_connection == null)
-				_connection = _mysqlConnectionString!;
-			return _connection;
-		}
-	}
 
 	public ConfigParser(){
 		_mongoConnectionString = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING");
-		_mysqlConnectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING");
-		_databaseName = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
 		_mongoDBName = Environment.GetEnvironmentVariable("MONGO_DATABASE");
-		if (_mongoConnectionString == null || _mysqlConnectionString == null || _databaseName == null)
+		if (_mongoConnectionString == null || _mongoDBName == null)
 		{
 			throw new Exception($"Can't find information to connect to databases:\n"+
-									$" |-mongo:{_mongoConnectionString}\n"+
-									$" |-mysql:{_mysqlConnectionString}\n"+
-									$" |-dbname: {_databaseName}"
+									$" |-mongo:{_mongoConnectionString}\n"
 								);
 		}
-		if (_mongoDBName == null)
-			_mongoDBName = _databaseName;
 	}
 	
 	public MongoDbSettings GetMongoDbSettings()
